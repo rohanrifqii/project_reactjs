170a7c2802c998f6d987c8f0a4fafc995a43d713
Raihan040107 <harryhand425@gmail.com>
2026-03-28 23:45:17 +0700

    Membuat halaman amar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import ProfilRohan from './page/ProfilRohan';
-import ProfilArva from './page/ProfilArva';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProfilRohan from "./page/ProfilRohan";
+import ProfilArva from "./page/ProfilArva";
+import ProfilAmar from "./page/ProfilAmar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50">
-            <h1 className="text-4xl font-extrabold text-slate-800 mb-4">Project Based Task - ReactJS</h1><br />
+        <Route
+          path="/"
+          element={
+            <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50">
+              <h1 className="text-4xl font-extrabold text-slate-800 mb-4">Project Based Task - ReactJS</h1>
+              <br />
 
-            <div className="flex flex-col sm:flex-row gap-4">
-              <Link
-                to="/rohan"
-                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-8 rounded-full shadow-lg transition-all hover:scale-105 text-center"
-              >
-                Profil Rohan
-              </Link>
-              <Link
-                to="/arva"
-                className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-3 px-8 rounded-full shadow-lg transition-all hover:scale-105 text-center"
-              >
-                Profil Arva
-              </Link>
+              <div className="flex flex-col sm:flex-row gap-4">
+                <Link to="/rohan" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-8 rounded-full shadow-lg transition-all hover:scale-105 text-center">
+                  Profil Rohan
+                </Link>
+                <Link to="/arva" className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-3 px-8 rounded-full shadow-lg transition-all hover:scale-105 text-center">
+                  Profil Arva
+                </Link>
+                <Link to="/amar" className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-3 px-8 rounded-full shadow-lg transition-all hover:scale-105 text-center">
+                  Profil Rama
+                </Link>
+              </div>
             </div>
-          </div>
-        } />
+          }
+        />
         <Route path="/rohan" element={<ProfilRohan />} />
         <Route path="/arva" element={<ProfilArva />} />
+        <Route path="/amar" element={<ProfilAmar />} />
       </Routes>
     </BrowserRouter>
   );
